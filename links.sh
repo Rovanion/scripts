f6a9cf34d1fbb4909486d8e5415f6cf385dd2476
Rovanion Luckey <rovanion.luckey@gmail.com>
2012-03-01 12:20:50 +0100

    minor correction

---
+++ links.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+This script requires suitable POSIX environment with wget, and awk installed.
+
+#For each URL in stdin
+for URL in $*
+do
+
+#Download the site and save it to hemsidan
+wget $URL -O hemsidan
+touch raderna
+#Grep out the lines that contain links and output them to the file raderna
+grep href hemsidan | grep -v $URL | grep http:// > raderna
+grep src hemsidan | grep -v $URL | grep http:// >> raderna
+
+#We then want to strip out the parts of the lines that are not part of the FQDN
+#First awk takes what's after :// and puts it into the file bags
+awk -F "://" '{print $2 > "bags"}' raderna
+#Then cut takes the first part before the " delimiter
+cut -f1 -d\" bags > bags1
+#Then cut takes the first part before the / delimiter
+cut -f1 -d/ bags1 > bags3
+#We then sort the list and only keep the unique entries
+sort bags3 | uniq > bags4
+#After that, output the result.
+echo $URL länkar vidare till följande sidor:
+cat bags4
+
+rm bags* hemsidan raderna
+
+done
